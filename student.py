b9d95fcd835d615973d5b6744bd0c20ce96a65f0
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2025-05-19 09:45:06 +0200

    learn metódus

--- student.py
+++ student.py
@@ -7,6 +7,9 @@ class Student:
     def introduce(self):
         print (f"Név: {self.name}, Kor: {self.age}, Nem: {self.sex}, Pontszám: {self.score}")
 
+    def learn(self, points):
+        self.score += points
+
 tivadar = Student()
 #print(tivadar)
 
@@ -16,4 +19,6 @@ tivadar.sex = "male"
 tivadar.score = 20
 
 #print (f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score}")
+tivadar.introduce()
+tivadar.learn(12)
 tivadar.introduce()
