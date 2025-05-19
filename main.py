6e00a03cf4ea4b7663769bf007c354abfc73b90d
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2025-05-19 10:40:02 +0200

    külső fájlból Student példányosítása

---
+++ main.py
@@ -0,0 +1,12 @@
+from student import Student
+
+tivadar = Student("Rontó Tivadar", "male", 16)
+leila = Student("Pusztító Leila", "female")
+
+tivadar.introduce()
+tivadar.learn(12)
+tivadar.introduce()
+
+leila.introduce()
+leila.learn(-2)
+leila.introduce()
--- student.py
+++ student.py
@@ -13,13 +13,3 @@ class Student:
     def learn(self, points):
         self.score += points
 
-tivadar = Student("Rontó Tivadar", "male", 16)
-leila = Student("Pusztító Leila", "female")
-
-tivadar.introduce()
-tivadar.learn(12)
-tivadar.introduce()
-
-leila.introduce()
-leila.learn(-2)
-leila.introduce()
