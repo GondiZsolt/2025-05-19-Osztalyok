69c084425e36641489c5545893d487b1fd9ae07b
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2025-05-19 10:17:57 +0200

    konstruktor hozzáadása

--- student.py
+++ student.py
@@ -1,8 +1,11 @@
 class Student:
-    name = ""
-    age = 0
-    sex = ""
-    score = 0
+
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.score = 10
+
 
     def introduce(self):
         print (f"Név: {self.name}, Kor: {self.age}, Nem: {self.sex}, Pontszám: {self.score}")
@@ -10,15 +13,8 @@ class Student:
     def learn(self, points):
         self.score += points
 
-tivadar = Student()
-#print(tivadar)
+tivadar = Student("El Tivadar", 16, "male")
 
-tivadar.name = "El tivadar"
-tivadar.age = 16
-tivadar.sex = "male"
-tivadar.score = 20
-
-#print (f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score}")
 tivadar.introduce()
 tivadar.learn(12)
 tivadar.introduce()
