cf7855af82b43bd2a7a245856d7eee5cf4eb011e
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2025-05-19 10:29:22 +0200

    fixed

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
 class Student:
 
-    def __init__(self, name, sex age = 0):
+    def __init__(self, name, sex, age = 0):
         self.name = name
         self.age = age
         self.sex = sex
@@ -13,11 +13,13 @@ class Student:
     def learn(self, points):
         self.score += points
 
-tivadar = Student("Rontó Tivadar", 16, "male")
+tivadar = Student("Rontó Tivadar", "male", 16)
 leila = Student("Pusztító Leila", "female")
 
 tivadar.introduce()
 tivadar.learn(12)
 tivadar.introduce()
 
-leila.introduce
+leila.introduce()
+leila.learn(-2)
+leila.introduce()
